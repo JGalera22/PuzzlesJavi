48ec3fd76247528943ab6efde453e2f566e82345
Javier García-Galera Rodríguez <galera221412@gmail.com>
2021-03-16 13:29:01 +0100

    Subida inicial

---
+++ Backend/Puzzles-Javi/src/main/kotlin/com/salesianostriana/dam/PuzzlesJavi/PuzzlesJaviApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.PuzzlesJavi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PuzzlesJaviApplication
+
+fun main(args: Array<String>) {
+	runApplication<PuzzlesJaviApplication>(*args)
+}
---
+++ Backend/Puzzles-Javi/src/test/kotlin/com/salesianostriana/dam/PuzzlesJavi/PuzzlesJaviApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.PuzzlesJavi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PuzzlesJaviApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
