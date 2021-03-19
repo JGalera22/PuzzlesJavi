b398d2b1414e73083e4c0a2783ba847f4c1ecd88
JGalera22 <galera221412@gmail.com>
2021-03-19 13:10:14 +0100

    API funciona, falta Postman y metodos de Linea de Pedido

--- Backend/Puzzles-Javi/src/main/kotlin/com/salesianostriana/dam/PuzzlesJavi/entities/dto/DTOPedido.kt
+++ Backend/Puzzles-Javi/src/main/kotlin/com/salesianostriana/dam/PuzzlesJavi/entities/dto/DTOPedido.kt
@@ -1,13 +1,18 @@
 package com.salesianostriana.dam.PuzzlesJavi.entities.dto
 
 import com.salesianostriana.dam.PuzzlesJavi.entities.Pedido
+import com.salesianostriana.dam.PuzzlesJavi.entities.Usuario
 import java.time.LocalDate
 
 data class GetPedidoDto (
-    var id: Long,
+    var id: Long?,
     var fechaPedido: LocalDate = LocalDate.now(),
     var total: Long,
 )
+
+fun Pedido.toGetPedidoDto(usuario: Usuario?):GetPedidoDto{
+    return GetPedidoDto(id, fechaPedido, total)
+}
 
 
 
