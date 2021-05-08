956189cfa89babfd2908857a3d77851261de39f8
Javier García-Galera Rodríguez <galera221412@gmail.com>
2021-05-09 00:44:46 +0200

    Arreglado el problema con el filtro de busqueda en admin, y la vuelta al MainActivity

---
+++ Frontend/Puzzles_Javi/app/src/main/java/com/naturesecurityvanguard/puzzles_Javi/ui/Admin/PantallaInicioAdmin/FilterActivityAdmin.kt
@@ -0,0 +1,51 @@
+package com.naturesecurityvanguard.puzzles_Javi.ui.Admin.PantallaInicioAdmin
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.naturesecurityvanguard.puzzles_Javi.MainActivity
+import com.naturesecurityvanguard.puzzles_Javi.R
+import com.naturesecurityvanguard.puzzles_Javi.ui.Admin.Main_Admin.AdminActivity
+
+
+class FilterActivityAdmin : AppCompatActivity() {
+    lateinit var categoriaSpiner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter_admin)
+
+        categoriaSpiner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.numero_piezas_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            categoriaSpiner.adapter = adapter
+        }
+
+        val boton:Button = findViewById(R.id.button_filtrar)
+
+        boton.setOnClickListener(View.OnClickListener {
+
+            var intent = Intent(this, AdminActivity::class.java)
+            this.startActivity(intent)
+
+            val categoria: String = categoriaSpiner.selectedItem.toString().toLowerCase()
+
+            val sharedPref = this.getSharedPreferences("FILE_PREFERENCES", Context.MODE_PRIVATE)
+            with(sharedPref.edit()) {
+                putString("categoria", categoria)
+                commit()
+            }
+        })
+
+    }
+
+}
